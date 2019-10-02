4dd84ef3aecdef3de1d7263b4403ca6a8f34f11b
santiagohenao97 <santiago.henao@hotmail.com>
2019-10-02 08:13:59 -0500

    full stack

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -6,21 +6,21 @@ const Hello= () => (
         <div className="row ">
             <div className="col-sm-6 ">
                 <h1 className="display-4 ml-3">Hello, I am Santiago!</h1>
-                <p className="lead ml-3">Electronic Engineer and EngD. Candidate at UPB<br/> My alter-ego is a FrontEnd Developer and I love it…</p>
+                <p className="lead ml-3">Electronic Engineer and EngD. Candidate at UPB<br/> My alter-ego is a Full Stack Developer and I love it…</p>
                 <hr className="my-4 bg-light"/>
                 <p className="ml-3">Do not think twice and let's do it together.</p>
                 <a className="btn btn-primary btn-lg ml-3" href="#ContactMe" role="button">Contact Me!</a>
             </div>
             <div className="col-sm-6 pt-3">
 
-                <div className="tagline3a"><strong>var </strong>{'personalInfo= {'}</div>
+                <div className="tagline3b"><strong>var </strong>{'personalInfo= {'}</div>
                 <br/>
                 <div className="tagline3b">"firstName": "Santiago",
                 <br/> "lastName": "Henao",
                 <br/> "age" :  22,
                 <br/> "location" : "Medellín"
                 </div>
-                <div className="tagline3a"> <br/>{'}'}</div>
+                <div className="tagline3b"> <br/>{'}'}</div>
             </div>
         </div>
     </div>
